782e7a321a50cf02b902f22050237fe6e6e23a85
Pavel Pegov <pashapegov31@gmail.com>
2026-08-01 18:04:50 +0300

    add config

---
+++ app/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL = ""
+    SECRET_KEY = ""
+    ALGORITH = ""
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+settings = Settings()
