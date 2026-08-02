f4794b2c5e55ef44eb914cc7d8f187b2f9e0df1c
Pavel Pegov <pashapegov31@gmail.com>
2026-08-02 12:44:13 +0300

    add __init__ and update places and users models

--- app/models/places.py
+++ app/models/places.py
@@ -1,6 +1,7 @@
-from app.database import Base
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
 
 
 class Places(Base):
--- app/models/users.py
+++ app/models/users.py
@@ -1,8 +1,7 @@
-from app.database import Base
-from sqlalchemy import Boolean, String, Text
-
-
+from sqlalchemy import Boolean, ForeignKey, String, Text
 from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
 
 
 class Users(Base):
@@ -11,6 +10,6 @@ class Users(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
     hashed_password: Mapped[str] = mapped_column(Text)
-    tickets_ids: Mapped[list[int]] = mapped_column()  #! ForeignKey to Tickets
+    tickets_ids: Mapped[list[int]] = mapped_column(ForeignKey("tickets.id"))
     is_admin: Mapped[bool] = mapped_column(Boolean)
-    concerts_ids: Mapped[list[int]] = mapped_column()  #! ForeignKey to Concerts
+    concerts_ids: Mapped[list[int]] = mapped_column(ForeignKey("concerts.id"))
