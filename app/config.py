2935efc385f08b8e2691f90b7e48a2c3cf8e4e15
Pavel Pegov <pashapegov31@gmail.com>
2026-08-02 12:30:18 +0300

    settings

--- app/config.py
+++ app/config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL = ""
-    SECRET_KEY = ""
-    ALGORITH = ""
+    DATABASE_URL = "postrgres+aiosync://./afisha.db"
+    SECRET_KEY = "own-afisha-secret-key"
+    ALGORITM = "SH256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
